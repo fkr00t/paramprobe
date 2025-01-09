b879a3a585d48bc67b9f9ac4e311647a62ff27b3
fkr00t <fkr00t@duck.com>
2025-01-09 22:24:16 +0700

    Add Update and Version

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func printBanner() {
 ▌  ▞▀▌▌  ▞▀▌▌▐ ▌▌  ▌  ▌ ▌▌ ▌▛▀ 
 ▘  ▝▀▘▘  ▝▀▘▘▝ ▘▘  ▘  ▝▀ ▀▀ ▝▀▘
 
-    Version: 1.0.0
+    Version: 2.0.0
     Reflected Parameter Finder
     Author: fkr00t | Github: https://github.com/fkr00t
     `
